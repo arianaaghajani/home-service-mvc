490443549d9267461d476d113fe39c93484c4f1a
arianaAghajani <aghajaniariana@gmail.com>
2024-08-01 23:18:15 +0330

    fix methods

--- src/main/java/ir/ariana/home_service_mvc/controller/AdminController.java
+++ src/main/java/ir/ariana/home_service_mvc/controller/AdminController.java
@@ -1,12 +1,12 @@
 package ir.ariana.home_service_mvc.controller;
 
-import ir.ariana.home_service_mvc.dto.MainServiceReturn;
-import ir.ariana.home_service_mvc.dto.MainServiceSaveRequest;
-import ir.ariana.home_service_mvc.dto.SubServiceReturn;
-import ir.ariana.home_service_mvc.dto.SubServiceSaveRequest;
+import ir.ariana.home_service_mvc.dto.*;
+import ir.ariana.home_service_mvc.enums.SpecialistStatus;
 import ir.ariana.home_service_mvc.mapper.MainServiceMapper;
+import ir.ariana.home_service_mvc.mapper.SpecialistMapper;
 import ir.ariana.home_service_mvc.mapper.SubServiceMapper;
 import ir.ariana.home_service_mvc.model.MainService;
+import ir.ariana.home_service_mvc.model.Specialist;
 import ir.ariana.home_service_mvc.model.SubService;
 import ir.ariana.home_service_mvc.service.*;
 import lombok.RequiredArgsConstructor;
@@ -77,4 +77,40 @@ public class AdminController {
                 HttpStatus.OK);
     }
 
+    @GetMapping("show_SubService_For_One_MainService")
+    public List<SubServiceReturn> showSubServiceForOneMainService(@RequestParam Long id) {
+        MainService mainService = mainServiceService.findById(id);
+        List<SubService> subServices = subServiceService.findBySubService(mainService);
+        return SubServiceMapper.INSTANCE.listSubServiceToSaveResponse(subServices);
+    }
+
+    @GetMapping("make_Specialist_Accepted")
+    public ResponseEntity<SpecialistReturn> makeSpecialistAccepted(@RequestParam Long id, SpecialistStatus specialistStatus) {
+        Specialist specialist = specialistService.findById(id);
+        Specialist updated = specialistService.updateSpecialistStatus(specialistStatus, specialist);
+        return new ResponseEntity<>(SpecialistMapper.INSTANCE.modelSpecialistToSaveResponse(updated),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("add_Specialist_ToSubService")
+    public ResponseEntity<SpecialistReturn> addSpecialistToSubService(@RequestParam Long specialistId, Long subServiceId) {
+        Specialist specialist = specialistService.findById(specialistId);
+        Specialist addedSpecialist = adminService.addSpecialistToSubService(specialist, subServiceId);
+        return new ResponseEntity<>(SpecialistMapper.INSTANCE.modelSpecialistToSaveResponse(addedSpecialist),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("remove_Specialist_SubService")
+    public ResponseEntity<SpecialistReturn> removeSpecialistSubService(@RequestParam Long id, Long subServiceId) {
+        Specialist specialist = specialistService.findById(id);
+        Specialist specialist1 = adminService.removeSpecialistFromSubService(specialist, subServiceId);
+        return new ResponseEntity<>(SpecialistMapper.INSTANCE.modelSpecialistToSaveResponse(specialist1),
+                HttpStatus.OK);
+    }
+
+    @DeleteMapping("delete_mainService")
+    public String removeMainService(@RequestParam Long id) {
+        mainServiceService.removeMainService(id);
+        return "mainService and All relations have been deleted";
+    }
 }
